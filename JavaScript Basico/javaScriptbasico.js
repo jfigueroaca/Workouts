19fcccd9d7a0b4b26995cf56b0c8a66a43890321
jfigueroaca <jfigueroaca@gmail.com>
2022-09-01 09:34:53 -0500

    :sparkles: Agregué logo

---
+++ JavaScript Basico/javaScriptbasico.js
@@ -0,0 +1,11 @@
+var edad = 18;
+
+if(edad===18) {
+    console.log("Puedes votar por primera vez");
+}
+else if (edad>18){
+    console.log("Puedes votar de nuevo");
+}
+else(edad<18){
+    console.log("Aún no puedes votar");
+}
